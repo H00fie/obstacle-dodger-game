e160e1613c2753c367f0e8d8e5d458f1ca060416
Dazikiri Anikar <mierzwinski.b@gmail.com>
2023-01-08 16:18:07 +0100

    Added the rest of the obstacles and finished the initial version of the game.

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
-    [SerializeField] float xAngle = 0;
-    [SerializeField] float yAngle = 0;
-    [SerializeField] float zAngle = 0;
+    // [SerializeField] float xAngle = 0;
+    // [SerializeField] float yAngle = 0.5f;
+    // [SerializeField] float zAngle = 0;
     
 
     // Update is called once per frame
@@ -14,6 +14,8 @@ public class Spinner : MonoBehaviour
     {
         //I am reaching for the 'transform' section of the object the script is attached to and grabbing the 'Rotate' property.
         //'transform' is a class in Unity.
-        transform.Rotate(xAngle, yAngle, zAngle);
+        //Referring to the variables above doesn't work, upon the start of the game, the Y remains unchanged. It works if I hardcode
+        //the values.
+        transform.Rotate(0, 0.5f, 0);
     }
 }
